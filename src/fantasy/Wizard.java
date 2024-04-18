00c28f643e36714b3b444b3c87005c9afa6faec4
esh <z9901z@naver.com>
2024-04-18 16:14:27 +0900

    다시 커밋

--- src/fantasy/Wizard.java
+++ src/fantasy/Wizard.java
@@ -1,23 +1,51 @@
-package fantazy;
+package fantasy;
 
 public class Wizard {
 	// Wizard 클래스 변수 설정
 	private String name; // 이름
 	private int health; // 체력
 	private int power; // 공격력
-
-	// Warrior 공격할 때
-	public void attackWarrior(Warrior warrior) {
-		int a = warrior.getHealth();
-		if (warrior.health <= 0) {
-			System.out.println(warrior.getName + "가 사망했습니다.");
-			warrior.setHealth(0);
-			return;
+	
+	public Wizard(String name) {
+		this.name=name;
+		health=100;
+		power=30;
+	}
+	
+	// getter 메서드
+		public String getName() { // getName
+			return this.name;
 		}
-		warrior.setHealth(a - power);
+
+		public int getHealth() {
+			return this.health;
+		}
+
+		public int getPower() {
+			return power;
+		}
+
+		// setter 메서드
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public void setHealth(int health) {
+			this.health = health;
+		}
+
+		public void setPower(int power) {
+			this.power = power;
+		}
+	
+
+	// 마법사가 전사를 공격함
+	public void attackWarrior(Warrior warrior) {
+		warrior.beAttacked(this.power);
+		System.out.println();
 	}
 
-	// Thief 공격할 때
+	// 마법사가 도적을 공격함
 	public void attackThief(Thief thief) {
 		int a = thief.getHealth();
 		if (a <= 0) {
@@ -28,7 +56,7 @@ public class Wizard {
 		thief.setHealth(a - power);
 	}
 
-	// Archer 공격할 때
+	// 마법사가 궁수를 공격함
 	public void attackArcher(Archer archer) {
 		int a = archer.getHealth();
 		if (a <= 0) {
@@ -49,30 +77,5 @@ public class Wizard {
 		this.power -= power;
 	}
 
-	// getter 메서드
-	public String getName() { // getName
-		return this.name;
-	}
-
-	public int getHealth() {
-		return this.health;
-	}
-
-	public int getPower() {
-		return power;
-	}
-
-	// setter 메서드
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setHealth(int health) {
-		this.health = health;
-	}
-
-	public void setPower(int power) {
-		this.power = power;
-	}
 
 }
